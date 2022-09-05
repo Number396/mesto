c1d751f50fd3909125b3a2ac4208fa88b6c9394b
Number396 <number396@yandex.ru>
2022-09-06 00:24:45 +0300

    feat: add eventhadler

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const profile = document.querySelector(".profile");
-const profileEditBtn = profile.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const popupCloseBtn = popup.querySelector(".popup__close-button");
-const profileTitle = profile.querySelector(".profile__title");
-const profileSubtitle = profile.querySelector(".profile__subtitle");
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  const popup = document.querySelector(".popup");
+  const inputName = popup.querySelector(".input__text_type_name");
+  const inputOccupation = popup.querySelector(".input__text_type_occupation");
+  const profileTitle = document.querySelector(".profile__title");
+  const profileSubtitle = document.querySelector(".profile__subtitle");
 
-let textProfileTitle = profileTitle.textContent;
-let textProfileSubtitle = profileSubtitle.textContent;
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputOccupation.value;
+  popup.classList.toggle("popup-show");
+}
 
-let inputName = popup.querySelector(".input__text_type_name");
-let inputOccupation = popup.querySelector(".input__text_type_occupation");
+const formElement = document.querySelector("form");
+formElement.addEventListener("submit", formSubmitHandler);
 
-inputName.value = textProfileTitle;
-inputOccupation.value = textProfileSubtitle;
-console.log(inputName.value);
-console.log(inputOccupation.value);
-
-// profileEditBtn.onclick = () => {
-//     popup.style.display = 'block';
-// }
-
+const profileEditBtn = document.querySelector(".profile__edit-button");
 profileEditBtn.addEventListener("click", () => {
-  popup.style.display = "block";
-  inputName.value = textProfileTitle;
-  inputOccupation.value = textProfileSubtitle;
-  console.log(inputName.value);
-  console.log(inputOccupation.value);
+  const popup = document.querySelector(".popup");
+  popup.classList.toggle("popup-show");
+  const inputName = popup.querySelector(".input__text_type_name");
+  const inputOccupation = popup.querySelector(".input__text_type_occupation");
+  const profileTitle = document.querySelector(".profile__title");
+  const profileSubtitle = document.querySelector(".profile__subtitle");
+  inputName.value = profileTitle.textContent;
+  inputOccupation.value = profileSubtitle.textContent;
 });
 
+const popupCloseBtn = document.querySelector(".popup__close-button");
 popupCloseBtn.addEventListener("click", () => {
-  popup.style.display = "none";
+  const popup = document.querySelector(".popup");
+  popup.classList.toggle("popup-show");
 });
 
 // window.onclick = function(event) {
