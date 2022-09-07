72f5626fd2d0d76275f6dfbcf5bdb86dac85334b
Number396 <number396@yandex.ru>
2022-09-07 21:56:25 +0300

    style: add adaptive to the hole page

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
 const popup = document.querySelector(".popup");
 const profile = document.querySelector(".profile");
 const profileEditBtn = profile.querySelector(".profile__edit-button");
-const formElement = document.querySelector("form");
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-
-  const inputName = popup.querySelector(".input__text_type_name");
-  const inputOccupation = popup.querySelector(".input__text_type_occupation");
-  const profileTitle = profile.querySelector(".profile__title");
-  const profileSubtitle = profile.querySelector(".profile__subtitle");
-
-  profileTitle.textContent = inputName.value;
-  profileSubtitle.textContent = inputOccupation.value;
-  popup.classList.toggle("popup_enabled");
-}
+const profileTitle = profile.querySelector(".profile__title");
+const profileSubtitle = profile.querySelector(".profile__subtitle");
+const formElement = popup.querySelector("form");
+const popupCloseBtn = popup.querySelector(".popup__close-button");
+const inputName = popup.querySelector(".input__text_type_name");
+const inputOccupation = popup.querySelector(".input__text_type_occupation");
 
 profileEditBtn.addEventListener("click", () => {
-  popup.classList.toggle("popup_enabled");
-  const inputName = popup.querySelector(".input__text_type_name");
-  const inputOccupation = popup.querySelector(".input__text_type_occupation");
-  const profileTitle = profile.querySelector(".profile__title");
-  const profileSubtitle = profile.querySelector(".profile__subtitle");
+  popupOpen();
   inputName.value = profileTitle.textContent;
   inputOccupation.value = profileSubtitle.textContent;
 });
 
-const popupCloseBtn = popup.querySelector(".popup__close-button");
 popupCloseBtn.addEventListener("click", () => {
-  popup.classList.toggle("popup_enabled");
+  popupClose();
 });
 
-formElement.addEventListener("submit", formSubmitHandler);
+formElement.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputOccupation.value;
+  popupClose();
+});
+
+function popupOpen() {
+  popup.classList.add("popup_enabled");
+}
+
+function popupClose() {
+  popup.classList.remove("popup_enabled");
+}
