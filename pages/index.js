d848fcb7a7c0d5f34b4ec6172f2c3632561afc9f
Number396 <number396@yandex.ru>
2022-11-13 13:20:57 +0300

    refactor: add constants.js

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,11 @@
 import { Card } from "../scripts/Card.js";
 import { FormValidator } from "../scripts/FormValidator.js";
-const popupEdit = document.querySelector(".popup_edit-profile");
-const popupPlace = document.querySelector(".popup_add-place");
-const popupImage = document.querySelector(".popup_show-image");
-const profile = document.querySelector(".profile");
-const profileEditBtn = profile.querySelector(".profile__edit-button");
-const profileAddBtn = profile.querySelector(".profile__add-button");
-const profileTitle = profile.querySelector(".profile__title");
-const profileSubtitle = profile.querySelector(".profile__subtitle");
-const formEditElement = popupEdit.querySelector("form");
-const formAddElement = popupPlace.querySelector("form");
-const inputName = popupEdit.querySelector(".popup__input_type_name");
-const inputOccupation = popupEdit.querySelector(".popup__input_type_occupation");
-const inputPlace = popupPlace.querySelector(".popup__input_type_place");
-const inputLink = popupPlace.querySelector(".popup__input_type_link");
-const cardsItems = document.querySelector(".cards__items");
-const imgSrc = popupImage.querySelector(".popup__image");
-const imgFigure = popupImage.querySelector(".popup__caption");
-
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
-const settings = {
-  formSelector: '.popup__form',
-  fieldsetSelector: '.popup__set',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active'
-};
+import {
+  popupEdit, popupPlace, popupImage, profile, profileEditBtn, profileAddBtn,
+  profileTitle, profileSubtitle, formEditElement, formAddElement, inputName,
+  inputOccupation, inputPlace, inputLink, cardsItems, imgSrc, imgFigure,
+  initialCards, settings
+} from '../utils/constants.js';
 
 function setCloseBtnListeners() {
   const closeButtons = document.querySelectorAll(".popup__close-button");
---
+++ utils/constants.js
@@ -0,0 +1,52 @@
+export const popupEdit = document.querySelector(".popup_edit-profile");
+export const popupPlace = document.querySelector(".popup_add-place");
+export const popupImage = document.querySelector(".popup_show-image");
+export const profile = document.querySelector(".profile");
+export const profileEditBtn = profile.querySelector(".profile__edit-button");
+export const profileAddBtn = profile.querySelector(".profile__add-button");
+export const profileTitle = profile.querySelector(".profile__title");
+export const profileSubtitle = profile.querySelector(".profile__subtitle");
+export const formEditElement = popupEdit.querySelector("form");
+export const formAddElement = popupPlace.querySelector("form");
+export const inputName = popupEdit.querySelector(".popup__input_type_name");
+export const inputOccupation = popupEdit.querySelector(".popup__input_type_occupation");
+export const inputPlace = popupPlace.querySelector(".popup__input_type_place");
+export const inputLink = popupPlace.querySelector(".popup__input_type_link");
+export const cardsItems = document.querySelector(".cards__items");
+export const imgSrc = popupImage.querySelector(".popup__image");
+export const imgFigure = popupImage.querySelector(".popup__caption");
+export const settings = {
+    formSelector: '.popup__form',
+    fieldsetSelector: '.popup__set',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit-button',
+    inactiveButtonClass: 'popup__button_inactive',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active'
+};
+export const initialCards = [
+    {
+        name: "Архыз",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+        name: "Челябинская область",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+        name: "Иваново",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+        name: "Камчатка",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+        name: "Холмогорский район",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+        name: "Байкал",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+];
