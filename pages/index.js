afd229f23b6d1ef8cc255ad1826d6b1d8b878ba2
Number396 <number396@yandex.ru>
2022-11-18 21:54:15 +0300

    refactor: delete comments

--- pages/index.js
+++ pages/index.js
@@ -13,96 +13,6 @@ import {
 
 
 
-// function setCloseBtnListeners() {
-//   const closeButtons = document.querySelectorAll(".popup__close-button");
-
-//   closeButtons.forEach((button) => {
-//     const popup = button.closest(".popup");
-
-//     button.addEventListener("click", () => closePopup(popup));
-//   });
-// }
-
-// function setOverlayListeners() {
-//   const overlayList = document.querySelectorAll(".popup");
-
-//   overlayList.forEach((overlayElement) => {
-//     overlayElement.addEventListener("click", (evt) => {
-//       if (evt.target == overlayElement) closePopup(overlayElement);
-//     });
-//   });
-// }
-
-
-
-// function keyHandler(evt) {
-//   console.log(evt);
-//   if (evt.key === "Escape") {
-//     const openedPopup = document.querySelector(".popup_enabled");
-
-//     closePopup(openedPopup);
-//   }
-// }
-
-// function openPopup(popup) {
-//   popup.classList.add("popup_enabled");
-//   document.addEventListener("keydown", keyHandler);
-// }
-
-// function closePopup(popup) {
-//   popup.classList.remove("popup_enabled");
-//   document.removeEventListener("keydown", keyHandler);
-// }
-
-
-// function fillProfileFromInputs() {
-//   profileTitle.textContent = inputName.value;
-//   profileSubtitle.textContent = inputOccupation.value;
-// }
-// function handleOpenPopupImg(name, link) {
-//   imgSrc.src = link;
-//   imgSrc.alt = `Изображение ${name}.`;
-//   imgFigure.textContent = name;
-//   openPopup(popupImage);
-// };
-
-// function setSubmitBtnListeners() {
-//   formEditElement.addEventListener("submit", (evt) => {
-//     evt.preventDefault();
-//     fillProfileFromInputs();
-//     closePopup(popupEdit);
-//   });
-
-//   formAddElement.addEventListener("submit", (evt) => {
-//     evt.preventDefault();
-
-//     const cardData = [{ name: inputPlace.value, link: inputLink.value }];
-//     // const card = new Card(cardData, '#card-template', handleOpenPopup);
-//     // cardsItems.prepend(card.generateCard());
-//     const newCard = new Section({
-//       data: cardData,
-//       renderer: (item) => {
-//         const card = new Card(item, '#card-template', handleOpenPopup);
-//         const cardElement = card.generateCard();
-//         newCard.addItem(cardElement);
-//       }
-//     }, ".cards__items");
-//     newCard.renderItems();
-//     // todo: использовать метод close объекта
-//     closePopup(popupPlace);
-//   });
-// };
-
-
-
-// function renderElements() {
-//   initialCards.forEach((item) => {
-//     const card = new Card(item, '#card-template', handleOpenPopup);
-
-//     cardsItems.append(card.generateCard());
-//   });
-// }
-// renderElements();
 function handleOpenPopup(name, link) {
   const imgPopup = new PopupWithImage(".popup_show-image", name, link);
   imgPopup.open();
@@ -124,45 +34,13 @@ function setProfileBtnListeners() {
   profileEditBtn.addEventListener("click", () => {
     setInputs();
     setDefaultSettings(formEditElement);
-    // console.log('popup_edit-profile');
-    // const editPopup = new PopupWithForm({
-    //   popupSelector: ".popup_edit-profile",
-    //   handleFormSubmit: (formData) => {
-    //     console.log('name input: ', formData.nameInput);
-
-    //     profileTitle.textContent = formData.nameInput;
-    //     profileSubtitle.textContent = formData.occupationInput;
-
-    //     editPopup.close();
-    //     console.log(profileTitle.textContent);
-    //     console.log(profileSubtitle.textContent);
-    //   }
-    // });
     editPopup.open();
-    // openPopup(popupEdit);
   });
-
-  // const editPopup = new PopupWithForm({
-  //   popupSelector: ".popup_edit-profile",
-  //   handleFormSubmit: (formData) => {
-  //     // console.log('name input: ', formData.nameInput);
-
-  //     profileTitle.textContent = formData.nameInput;
-  //     profileSubtitle.textContent = formData.occupationInput;
-
-  //     editPopup.close();
-  //     // console.log(profileTitle.textContent);
-  //     // console.log(profileSubtitle.textContent);
-  //   }
-  // });
-
-  // editPopup.setEventListeners();
 
   profileAddBtn.addEventListener("click", () => {
     formAddElement.reset();
     setDefaultSettings(formAddElement);
     addPopup.open();
-    // openPopup(popupPlace);
   });
 };
 // -----------------------------------------------------------------------
@@ -172,10 +50,8 @@ const defaultCardList = new Section({
     const card = new Card(item, '#card-template', handleOpenPopup);
     const cardElement = card.generateCard();
     defaultCardList.addItem(cardElement);
-    // this.addItem(cardElement);
   }
 }, ".cards__items");
-
 defaultCardList.renderItems();
 
 // //мапа: (ключ: форма, значение: экземпляр класса валидации для этой формы)
@@ -225,11 +101,4 @@ const addPopup = new PopupWithForm({
 });
 addPopup.setEventListeners();
 
-
-// setSubmitBtnListeners();
 setProfileBtnListeners();
-// setCloseBtnListeners();
-// setOverlayListeners();
-
-// const closeButtons = document.querySelectorAll(".popup__close-button");
-// console.log(typeof (closeButtons));
