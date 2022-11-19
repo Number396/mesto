d58175ef9a142d8d1b3e1d1fbea368cd19111edb
Number396 <number396@yandex.ru>
2022-11-19 18:32:14 +0300

    refactor: move index.js to src folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { Card } from "../src/components/Card.js";
-import { FormValidator } from "../src/components/FormValidator.js";
-import { Section } from "../src/components/Section.js";
+import { Card } from "./components/Card.js";
+import { FormValidator } from "./components/FormValidator.js";
+import { Section } from "./components/Section.js";
 // import { Popup } from "../components/Popup.js";
-import { PopupWithImage } from "../src/components/PopupWithImage.js";
-import { PopupWithForm } from "../src/components/PopupWithForm.js";
-import { UserInfo } from "../src/components/UserInfo.js";
+import { PopupWithImage } from "./components/PopupWithImage.js";
+import { PopupWithForm } from "./components/PopupWithForm.js";
+import { UserInfo } from "./components/UserInfo.js";
 import {
   profileEditBtn,
   newcardAddBtn,
